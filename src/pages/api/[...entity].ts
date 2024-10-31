85fdcd45b94f5363523885589afc5a0dc466b1a8
bitkarrot <73979971+bitkarrot@users.noreply.github.com>
2024-10-30 23:52:37 -0700

    fix up old

--- src/pages/api/[...entity].ts
+++ src/pages/api/[...entity].ts
@@ -16,7 +16,7 @@ function parseTypeParam(endpoint: string | undefined) {
 /* Controllers */
 
 export const get: APIRoute = ({ params /* , request */ }) => {
-	console.log('Hit!', params.entity);
+//	console.log('Hit!', params.entity);
 
 	const operationName = parseTypeParam(params.entity);
 
